27a59d5b1f5ce614d5a06fd3599e8ed84a4a546d
Dmitry Azhichakov <diseaz@joom.com>
2020-07-18 13:38:53 +0300

    go-interpreter supports recursive functions

--- diseaz/interpreter/interpret_test.go
+++ diseaz/interpreter/interpret_test.go
@@ -1,33 +1,33 @@
 package interpreter
 
 import (
+	"log"
 	"reflect"
 	"testing"
 )
 
-func runProgram(t *testing.T, ts ...Token) Program {
+func runProgram(t *testing.T, ts ...Token) Token {
 	c := NewContext()
-	pout, err := Interpret(c, NewProgram(ts...))
+	p := NewProgram(ts...)
+	tok, err := Interpret(c, p)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("Interpret failed: %s", err)
 	}
-	return pout
+	log.Printf("Program: %s", tok)
+	r := tok.Eval(c)
+	return r
 }
 
-func testProgram(t *testing.T, expected Program, ts ...Token) Program {
-	c := NewContext()
-	pout, err := Interpret(c, NewProgram(ts...))
-	if err != nil {
-		t.Fatal(err)
+func testProgram(t *testing.T, expected Token, ts ...Token) Token {
+	r := runProgram(t, ts...)
+	if !reflect.DeepEqual(r, expected) {
+		t.Errorf("Result does not match: %s != %s", expected, r)
 	}
-	if !reflect.DeepEqual(pout, expected) {
-		t.Fail()
-	}
-	return pout
+	return r
 }
 
 func TestAdd(t *testing.T) {
-	testProgram(t, Program{Int{V: 42}},
+	testProgram(t, Int{V: 42},
 		Ap{},
 		Ap{},
 		Add{},
@@ -37,7 +37,7 @@ func TestAdd(t *testing.T) {
 }
 
 func TestMul(t *testing.T) {
-	testProgram(t, Program{Int{V: 42}},
+	testProgram(t, Int{V: 42},
 		Ap{},
 		Ap{},
 		Mul{},
@@ -46,16 +46,66 @@ func TestMul(t *testing.T) {
 	)
 }
 
+func TestDiv(t *testing.T) {
+	testProgram(t, Int{V: -6},
+		Ap{},
+		Ap{},
+		Div{},
+		Int{V: 43},
+		Int{V: -7},
+	)
+}
+
+func TestTrue(t *testing.T) {
+	testProgram(t, Int{V: 42},
+		Ap{},
+		Ap{},
+		True{},
+		Int{V: 42},
+		Int{V: 43},
+	)
+}
+
+func TestFalse(t *testing.T) {
+	testProgram(t, Int{V: 42},
+		Ap{},
+		Ap{},
+		False{},
+		Int{V: 43},
+		Int{V: 42},
+	)
+}
+
 func TestEq(t *testing.T) {
-	testProgram(t, Program{True{}},
+	testProgram(t, Int{V: 1},
+		Ap{},
+		Ap{},
+
 		Ap{},
 		Ap{},
 		Eq{},
+
 		Int{V: 6},
+
 		Ap{},
 		Ap{},
 		Div{},
 		Int{V: 42},
 		Int{V: 7},
+
+		Int{V: 1},
+		Int{V: 0},
+	)
+}
+
+func TestPwr2(t *testing.T) {
+	testProgram(t, Int{V: 1},
+		Ap{}, Pwr2{}, Int{V: 0},
+	)
+	testProgram(t, Int{V: 2},
+		Ap{}, Pwr2{}, Int{V: 1},
+	)
+	testProgram(t, Int{V: 16},
+		Ap{}, Pwr2{}, Int{V: 4},
 	)
 }
