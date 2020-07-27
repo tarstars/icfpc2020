d6123efb98e7702974c0403ab4e846f212ad868e
Arseniy Trushin <tass@thoth.travel>
2020-07-27 15:07:10 +0300

    solver class

--- src/interpreter/test_eval.py
+++ src/interpreter/test_eval.py
@@ -12,13 +12,10 @@ class IdGenerator:
         return result
 
 
-generator = IdGenerator()
-parents = {}
-
-
 class IdOperations:
-    def __init__(self, local_hash):
-        self.local_id = generator.get_id()
+    def __init__(self, local_hash, solver):
+        self.solver = solver
+        self.local_id = solver.generator.get_id()
         self.local_hash = local_hash
 
     def get_id(self):
@@ -34,15 +31,15 @@ class IdOperations:
 
     def __mul__(self, other):
         if isinstance(other, CNode) or isinstance(other, CLeaf):
-            return Node(self, other)
+            return self.solver.Node(self, other)
         if isinstance(other, int):
-            return Node(self, CLeaf(other))
+            return self.solver.Node(self, CLeaf(other, solver=self.solver))
         return NotImplemented
 
 
 class CNode(IdOperations):
-    def __init__(self, left=None, right=None):
-        super().__init__(local_hash=hash((left, right)))
+    def __init__(self,  solver, left=None, right=None):
+        super().__init__(local_hash=hash((left, right)), solver=solver)
         self.left = left  # a function
         self.right = right  # an argument
 
@@ -53,21 +50,12 @@ class CNode(IdOperations):
 
 
 class CLeaf(IdOperations):
-    def __init__(self, val):
-        super().__init__(hash(val))
+    def __init__(self, val, solver):
+        super().__init__(local_hash=hash(val), solver=solver)
         self.val = val
 
     def __repr__(self):
         return repr(self.val)
-
-
-def Node(left, right):
-    p = (left.get_id(), right.get_id())
-
-    if p not in parents:
-        parents[p] = CNode(left=left, right=right)
-
-    return parents[p]
 
 
 class NArn:
@@ -92,103 +80,138 @@ class NArn:
         return NArn(args=new_args, n_args=self.n_args, name=self.name, func=self.func)
 
 
-def Leaf(n_args, func, name):
-    return CLeaf(NArn(n_args=n_args, func=func, name=name))
-
-
 def get_leaf_name(y):
     return y.val.name if isinstance(y, CLeaf) and isinstance(y.val, NArn) else None
-
-
-def s_apply(ap, x, y, z):
-    x = ap[x]
-    xz = ap[(x, z)]
-    yz = z if get_leaf_name(y) == "I" else Node(y, z)
-
-    return ap[(xz, yz)]
-
-
-def i_apply(ap, x):
-    return ap[x]
-
-
-c_s = Leaf(n_args=3, func=s_apply, name="S")
-c_i = Leaf(n_args=1, func=i_apply, name="I")
-c_b = Leaf(n_args=3, func=lambda ap, x, y, z: ap[(ap[x], Node(y, z))], name="B")
-c_c = Leaf(n_args=3, func=lambda ap, x, y, z: ap[(ap[(ap[x], z)], y)], name="C")
-c_t = Leaf(n_args=2, func=lambda ap, x, y: ap[x], name="T")
-c_f = Leaf(n_args=2, func=lambda ap, x, y: ap[y], name="F")
-c_car = Leaf(n_args=1, func=lambda ap, x: ap[(ap[x], c_t)], name="car")
-c_cdr = Leaf(n_args=1, func=lambda ap, x: ap[(ap[x], c_f)], name="cdr")
-c_cons = Leaf(n_args=3, func=lambda ap, x, y, z: ap[(ap[(ap[z], x)], y)], name="cons")
-c_nil = Leaf(n_args=1, func=lambda ap, x: c_t, name="nil")
-c_neg = Leaf(n_args=1, func=lambda ap, x: -ap[x], name="neg")
-c_isnil = Leaf(n_args=1, func=lambda ap, x: c_t if get_leaf_name(ap[x]) == "nil" else c_f, name="isnil")
-c_eq = Leaf(n_args=2, func=lambda ap, x, y: c_t if ap[x] == ap[y] else c_f, name="eq")
-c_lt = Leaf(n_args=2, func=lambda ap, x, y: c_t if ap[x] < ap[y] else c_f, name="lt")
-c_mul = Leaf(n_args=2, func=lambda ap, x, y: ap[x] * ap[y], name="mul")
-c_add = Leaf(n_args=2, func=lambda ap, x, y: ap[x] + ap[y], name="add")
-c_div = Leaf(n_args=2, func=lambda ap, x, y: int(ap[x] / ap[y]), name="add")
-c_inc = Leaf(n_args=1, func=lambda ap, x: ap[x] + 1, name="inc")
-c_dec = Leaf(n_args=1, func=lambda ap, x: ap[x] - 1, name="dec")
 
 
 class OurKeyError(KeyError):
     pass
 
 
-def create_sii_test():
-    s1 = Node(c_s, c_i)
-    s2 = Node(s1, c_i)
+class Solver:
+    def __init__(self):
+        self.generator = IdGenerator()
+        self.parents = {}
 
-    s3 = Node(c_s, c_i)
-    s4 = Node(s3, c_i)
+        self.c_s = self.Leaf(
+            n_args=3,
+            func=lambda ap, x, y, z: ap[
+                (ap[(ap[x], z)], z if get_leaf_name(y) == "I" else self.Node(y, z))
+            ],
+            name="S",
+        )
+        self.c_i = self.Leaf(n_args=1, func=lambda ap, x: ap[x], name="I")
+        self.c_b = self.Leaf(
+            n_args=3, func=lambda ap, x, y, z: ap[(ap[x], self.Node(y, z))], name="B"
+        )
+        self.c_c = self.Leaf(
+            n_args=3, func=lambda ap, x, y, z: ap[(ap[(ap[x], z)], y)], name="C"
+        )
+        self.c_t = self.Leaf(n_args=2, func=lambda ap, x, y: ap[x], name="T")
+        self.c_f = self.Leaf(n_args=2, func=lambda ap, x, y: ap[y], name="F")
+        self.c_car = self.Leaf(
+            n_args=1, func=lambda ap, x: ap[(ap[x], self.c_t)], name="car"
+        )
+        self.c_cdr = self.Leaf(
+            n_args=1, func=lambda ap, x: ap[(ap[x], self.c_f)], name="cdr"
+        )
+        self.c_cons = self.Leaf(
+            n_args=3, func=lambda ap, x, y, z: ap[(ap[(ap[z], x)], y)], name="cons"
+        )
+        self.c_nil = self.Leaf(n_args=1, func=lambda ap, x: self.c_t, name="nil")
+        self.c_neg = self.Leaf(n_args=1, func=lambda ap, x: -ap[x], name="neg")
+        self.c_isnil = self.Leaf(
+            n_args=1,
+            func=lambda ap, x: self.c_t if get_leaf_name(ap[x]) == "nil" else self.c_f,
+            name="isnil",
+        )
+        self.c_eq = self.Leaf(
+            n_args=2,
+            func=lambda ap, x, y: self.c_t if ap[x] == ap[y] else self.c_f,
+            name="eq",
+        )
+        self.c_lt = self.Leaf(
+            n_args=2,
+            func=lambda ap, x, y: self.c_t if ap[x] < ap[y] else self.c_f,
+            name="lt",
+        )
+        self.c_mul = self.Leaf(
+            n_args=2, func=lambda ap, x, y: ap[x] * ap[y], name="mul"
+        )
+        self.c_add = self.Leaf(
+            n_args=2, func=lambda ap, x, y: ap[x] + ap[y], name="add"
+        )
+        self.c_div = self.Leaf(
+            n_args=2, func=lambda ap, x, y: int(ap[x] / ap[y]), name="add"
+        )
+        self.c_inc = self.Leaf(n_args=1, func=lambda ap, x: ap[x] + 1, name="inc")
+        self.c_dec = self.Leaf(n_args=1, func=lambda ap, x: ap[x] - 1, name="dec")
 
-    root = Node(s2, s4)
-    return root
+    def Node(self, left, right):
+        p = (left.get_id(), right.get_id())
 
+        if p not in self.parents:
+            self.parents[p] = CNode(solver=self, left=left, right=right)
 
-def create_s_inc_test():
-    return c_s * c_add * c_inc * 1
+        return self.parents[p]
 
+    def Leaf(self, n_args, func, name):
+        return CLeaf(
+            NArn(n_args=n_args, func=func, name=name), solver=self
+        )
 
-root = create_s_inc_test()
-todo = [root]
-todo_set = set(todo)
-cache: Dict[Union[CNode, tuple, CLeaf], Any] = {}
+    def create_sii_test(self):
+        return (self.c_s * self.c_i * self.c_i) * (self.c_s * self.c_i * self.c_i)
 
-while todo:
-    try:
-        current = todo[-1]
-        if isinstance(current, CNode):
-            left = current.left
+    def create_s_inc_test(self):
+        return self.c_s * self.c_add * self.c_inc * 1
 
+    def evaluate(self, root):
+        todo = [root]
+        todo_set = set(todo)
+        cache: Dict[Union[CNode, tuple, CLeaf], Any] = {}
+
+        while todo:
             try:
-                apply_task = (cache[left], current.right)
-                task_result = cache[apply_task]
-            except KeyError as ke:
-                raise OurKeyError(ke)
+                current = todo[-1]
+                if isinstance(current, CNode):
+                    left = current.left
 
-            cache[current] = task_result
-            todo.pop()
-        elif isinstance(current, tuple):
-            left, right = current
-            try:
-                value = left.apply(cache, right)
-            except KeyError as ke:
-                raise OurKeyError(ke)
-            cache[current] = value
-            logging.warning(f"{current} -> {value}")
-            todo.pop()
-        elif isinstance(current, CLeaf):
-            cache[current] = current.val
-            todo.pop()
-    except OurKeyError as oke:
-        vta = oke.args[0].args[0]
-        todo.append(vta)
-        if vta in todo_set:
-            raise ValueError("Simple infinite recursion detected")
-        todo_set.add(vta)
-        continue
+                    try:
+                        apply_task = (cache[left], current.right)
+                        task_result = cache[apply_task]
+                    except KeyError as ke:
+                        raise OurKeyError(ke)
 
-print(cache[root])
+                    cache[current] = task_result
+                    todo.pop()
+                elif isinstance(current, tuple):
+                    left, right = current
+                    try:
+                        value = left.apply(cache, right)
+                    except KeyError as ke:
+                        raise OurKeyError(ke)
+                    cache[current] = value
+                    logging.warning(f"{current} -> {value}")
+                    todo.pop()
+                elif isinstance(current, CLeaf):
+                    cache[current] = current.val
+                    todo.pop()
+            except OurKeyError as oke:
+                vta = oke.args[0].args[0]
+                todo.append(vta)
+                if vta in todo_set:
+                    raise ValueError("Simple infinite recursion detected")
+                todo_set.add(vta)
+                continue
+
+        return cache[root]
+
+
+def main():
+    solver = Solver()
+    print(solver.evaluate(solver.create_s_inc_test()))
+
+
+if __name__ == "__main__":
+    main()
